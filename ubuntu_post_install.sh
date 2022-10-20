c1700f79fe6b0689cc2a4e5a42c917b35fb6d3c0
Kshitiz Bhushan <kshitizbhushan01@gmail.com>
2022-10-20 16:31:59 +0530

    Update ubuntu_post_install.sh

--- ubuntu_post_install.sh
+++ ubuntu_post_install.sh
@@ -2,5 +2,7 @@
 sudo apt-get update; sudo apt-get upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
-sudo apt install vlc gimp blender timeshift kazam dconf-editor rhythmbox telegram-desktop gnome-tweaks neofetch python3-tk python-is-python3 nautilus-image-converter macchanger -y
+sudo apt install vlc gimp blender timeshift kazam dconf-editor rhythmbox flameshot timeshift ufw telegram-desktop steghide gnome-tweaks neofetch python3-tk python-is-python3 nautilus-image-converter macchanger chkrootkit rkhunter -y
 
+sudo chkrootkit -y
+sudo rkhunter -s --sk
