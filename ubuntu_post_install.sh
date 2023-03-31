73c48259c18a9b525e5dbc0f6f2b8d0accabddad
Kshitiz Bhushan <kshitizbhushan01@gmail.com>
2023-03-31 19:24:41 +0530

    Update ubuntu_post_install.sh

--- ubuntu_post_install.sh
+++ ubuntu_post_install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sudo apt update; sudo apt upgrade -y
-sudo apt autoremove -y
-sudo apt autoclean -y
+sudo apt update && sudo apt upgrade -y
 sudo apt install vlc gimp blender timeshift dconf-editor rhythmbox usbguard gnome-sushi flameshot timeshift ufw telegram-desktop steghide gnome-tweaks python3-nautilus nemo-python gir1.2-nautilus-3.0 sshfs neofetch python3-tk python-is-python3 nautilus-image-converter macchanger clamav kazam testdisk finger speedtest-cli chkrootkit fail2ban rkhunter -y
-
-sudo rkhunter -c --sk
+sudo apt autoremove && sudo apt autoclean -y
